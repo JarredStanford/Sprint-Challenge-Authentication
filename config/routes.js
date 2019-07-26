93c4c7daea5cc0d6256386ed43b3068b1d1e1554
unknown <stanj89@gmail.com>
2019-07-26 11:53:01 -0400

    login

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, jwtKey } = require('../auth/authenticate');
 
 const Users = require('./route-helpers.js')
 
@@ -34,6 +35,28 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  const { username, password } = req.body
+
+
+  Users.getUserByName(username)
+    .then(user => {
+
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({
+          message: `Welcome, ${user.username},`, token
+        })
+      } else {
+        res.status(401).json({
+          message: "Your username or password are incorrect."
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was an error logging in."
+      })
+    })
 }
 
 function getJokes(req, res) {
@@ -49,4 +72,17 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const jwtPayload = {
+    subject: user.id,
+    username: user.username
+  }
+
+  const jwtOptions = {
+    expiresIn: '7d'
+  }
+
+  return jwt.sign(jwtPayload, jwtKey, jwtOptions)
 }
